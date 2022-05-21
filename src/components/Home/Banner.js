feb979180b226aff6ceb2f15fa32dfb2e87e9fea
Sumonsnote <sohankhance@gmail.com>
2022-05-22 01:10:18 +0600

    banner setup

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import banner from '../../Images/car-banner.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div class="hero min-h-screen">
+            <div class="hero-content flex-col lg:flex-row">
+                <div>
+                    <h1 class="text-5xl font-bold">Welcome to Auto Car INC</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+                <img src={banner}class="max-w-sm rounded-lg shadow-2xl" alt='' />
+            </div>
         </div>
     );
 };
