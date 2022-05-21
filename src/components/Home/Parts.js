5f364d6541df9811bf077f458d2483077ac5ea26
Sumonsnote <sohankhance@gmail.com>
2022-05-22 04:21:05 +0600

    title bar under part

--- src/components/Home/Parts.js
+++ src/components/Home/Parts.js
@@ -11,6 +11,9 @@ const Parts = () => {
     return (
         <div className='my-20'>
             <h1 class="text-4xl font-bold text-center">Parts | Tools</h1>
+            <div className='text-center my-5'>
+                <progress class="progress progress-info w-56" value="100" max="100"></progress>
+            </div>
             <div className='flex justify-center gap-20 my-10'>
                 {
                     parts.map(part => <PartsDetail part={part}></PartsDetail>)
